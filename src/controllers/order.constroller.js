0f52782da5939de47940aa548169fa8e839336f7
m4arthu <luisarthir259@gmail.com>
2023-11-30 16:11:25 -0300

    fix: order without response

--- src/controllers/order.constroller.js
+++ src/controllers/order.constroller.js
@@ -1,7 +1,8 @@
 import {orderSerivce} from '../services/order.service.js';
 const createOrder = async (req, res) => {
   const data = req.body;
-  return await orderSerivce.createOrder(data);
+  const order =  await orderSerivce.createOrder(data);
+  return res.status(201).send(order);
 };
 
 export const orderController = {
