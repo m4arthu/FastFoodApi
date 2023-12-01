053a3e7ba2a465682aef75e632fdd29210d0d42c
m4arthu <luisarthir259@gmail.com>
2023-11-30 22:24:46 -0300

    feat: integration tests

---
+++ tests/factorys/products.factory.js
@@ -0,0 +1,15 @@
+import { prisma } from "../../prisma/prisma.js"
+import { faker } from "@faker-js/faker"
+export const createProducts = async (numero) => {
+   const products = []
+   for (let i = 1; i <= numero; i++) {
+      const data = {
+         name: faker.word.adverb(),
+         price: faker.number.int(1000, 20000),
+         description: "Muiitoobommm"
+      }
+      const product = await prisma.product.create({ data })
+      products.push(product)
+   }
+   return products
+}
