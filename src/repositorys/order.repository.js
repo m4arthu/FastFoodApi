135caae6c80e8a07e3423b6b2a47fa468fee89f3
m4arthu <luisarthir259@gmail.com>
2023-11-30 23:41:43 -0300

    feat: qunatity added to order

--- src/repositorys/order.repository.js
+++ src/repositorys/order.repository.js
@@ -1,17 +1,17 @@
-import { prisma } from '../../prisma/prisma.js';
+import {prisma} from '../../prisma/prisma.js';
 
 const createOrder = async (data) => {
   return await prisma.order.create({
     data: {
       ...data, productId: Number(data.productId),
-      quantity: Number(data.quantity)
+      quantity: Number(data.quantity),
     },
   });
 };
 
 const updateOrder = async (data) => {
   return await prisma.order.update({
-    where: { id: data.orderId },
+    where: {id: data.orderId},
     data: {
       isFinished: Boolean(data.isFinished),
     },
@@ -20,7 +20,7 @@ const updateOrder = async (data) => {
 
 const getOrderById = async (orderId) => {
   return await prisma.order.findFirst({
-    where: { id: orderId },
+    where: {id: orderId},
   });
 };
 
