e402371c42fcefa4033cc35230413a42781cbf26
m4arthu <luisarthir259@gmail.com>
2023-12-04 14:38:53 -0300

    feat: sweagger documentation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express, {json} from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import sweagerUI from "swagger-ui-express"
 import {appRoutes} from './routes/app.routes.js';
 import {errorHandler} from './middleweres/errorHandler.middlewere.js';
+// eslint-disable-next-line
+import swaggerJson from '../sweagger.json' assert { type: 'json' };
+
 
 export const app = express();
 app.use(json());
 app.use(cors());
 app.use('/', appRoutes);
+app.use('/api-docs',sweagerUI.serve,sweagerUI.setup(swaggerJson))
 app.use(errorHandler);
