fbf0ff6b4bb3f437db7eb429a448676863fb2ed0
m4arthu <luisarthir259@gmail.com>
2023-12-04 19:39:23 -0300

    fear: side efectis in  tests

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import express, {json} from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import sweagerUI from "swagger-ui-express"
+import sweagerUI from 'swagger-ui-express';
 import {appRoutes} from './routes/app.routes.js';
 import {errorHandler} from './middleweres/errorHandler.middlewere.js';
-// import swaggerJson from '../sweagger.json' assert { type: 'json' };
+import swaggerJson from '../sweagger.json' assert { type: 'json' };
 
 
 export const app = express();
 app.use(json());
 app.use(cors());
 app.use('/', appRoutes);
-// app.use('/api-docs',sweagerUI.serve,sweagerUI.setup(swaggerJson))
+app.use('/api-docs',sweagerUI.serve,sweagerUI.setup(swaggerJson))
 app.use(errorHandler);
