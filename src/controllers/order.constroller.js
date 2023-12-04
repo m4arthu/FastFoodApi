10ba6550c1a8be29adafdad3b3388902d2fb6be4
m4arthu <luisarthir259@gmail.com>
2023-12-04 01:11:47 -0300

    feat delete

--- src/controllers/order.constroller.js
+++ src/controllers/order.constroller.js
@@ -15,8 +15,15 @@ const getOrders =async (req, res) => {
   const orders = await orderSerivce.getOrders();
   res.send(orders);
 };
+
+const  deleteOrder = async (req,res) => {
+  const orderId =  req.params.id
+  const deleteOrder =  await orderSerivce.deleteOrder(Number(orderId))
+  res.status(204).send()
+}
 export const orderController = {
   createOrder,
   updateOrder,
   getOrders,
+  deleteOrder
 };
--- src/middleweres/errorHandler.middlewere.js
+++ src/middleweres/errorHandler.middlewere.js
@@ -4,8 +4,10 @@ export const errorHandler = (err, req, res, next) => {
     return;
   }
   console.log(err);
-  if (err.code ==='P2003') {
+  if (err.code ==='P2003' || err.code === 'P2025') {
     res.status(404).send(err.message);
+    return
   }
+
   res.status(500).send('algo de errado aconteceu');
 };
